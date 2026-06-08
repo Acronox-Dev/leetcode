0e9183a3b402a217f1dcf74607f558ab579537ed
BARET Alexis <alexis.baret@imt-atlantique.net>
2026-06-08 06:00:06 +0400

    Easy solution for problem 9

---
+++ algorithms/9/9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 : return False
+
+        s = str(x)
+        n = len(s)
+
+        for i in range(n//2):
+            if s[i] != s[n - i - 1] :
+                return False
+        
+        return True
