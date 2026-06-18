957ed50cd1f6d400c200acef4d7881de81ac8282
BARET Alexis <alexis.baret@imt-atlantique.net>
2026-06-18 15:02:21 +0400

    Problem 108 solved using a recursion

---
+++ solutions/algorithms/108/108.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums : return None
+        
+        n = len(nums)
+        m = n // 2
+
+        left = self.sortedArrayToBST(nums[:m])
+        right = self.sortedArrayToBST(nums[(m+1):])
+
+        return TreeNode(nums[m],left,right)
