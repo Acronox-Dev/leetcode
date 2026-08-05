a9bb881a1d4973b345c11d71da809fa6bc84b221
BARET Alexis <alexis.baret@imt-atlantique.net>
2026-08-05 10:44:12 +0400

    Solved problem 11

--- solutions/algorithms/11/11.py
+++ solutions/algorithms/11/11.py
@@ -1,9 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        heightDistance = []
+        # Initialization
         n = len(height)
+        p1 = 0
+        p2 = n - 1
+        max = 0
 
-        for i in range(n):
-            heightDistance.append(i * height[i])
+        # Loop
+        while(p1 < p2) :
+            v1, v2 = height[p1], height[p2]
+
+            temp = min(v1,v2) * (p2 - p1)
+            if temp > max : max = temp
+            
+            if v2 > v1 : p1 += 1
+            else : p2 -= 1
+
+        return max
+
 
             
